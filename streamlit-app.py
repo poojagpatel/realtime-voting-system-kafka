909553151d7a029dba7b2546b7cbe6ae2669584f
Pooja Patel <poojapatel.3010@gmail.com>
2024-03-15 22:00:13 -0700

    display code for kpis of #voters and #candidates

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,9 +1,10 @@
 import psycopg2
 import streamlit as st
 import time
+from kafka import KafkaConsumer
+import simplejson as json
+import pandas as pd
 
-st.title('Realtime Election')
-st.write('Realtime Election Voting Dashboard')
 
 @st.cache_data
 def fetch_voting_stats():
@@ -20,9 +21,52 @@ def fetch_voting_stats():
 
     return voters_count, candidates_count
 
+
+def create_kafka_consumer(topic_name):
+    consumer = KafkaConsumer(
+        topic_name,
+        bootstrap_servers='localhost:9092',
+        auto_offset_rest='earliest',
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
+    return consumer
+
+
+def fetch_data_from_kafka(consumer):
+    messages = consumer.poll(timeout_ms=1000)
+    print(messages)
+    data = []
+    for message in messages.value():
+        for sub_message in message:
+            data.append(sub_message.value)
+    return data
+
+
 def update_data():
     last_refresh = st.empty()
-    last_refresh.text(f'Last refresh at: {time.strftime('%Y-%m-%d %H:%M:%S')} ')
+    last_refresh.text(f"Last refresh at: {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-    # fetch voting stats from postgres
-    fetch_voting_stats()
+    # Fetch voting stats from postgres
+    voters_count, candidates_count = fetch_voting_stats()
+
+    # Display statistics
+    st.markdown('---')
+    col1, col2 = st.columns(2)
+    col1.metric('Total Voters', voters_count)
+    col2.metric('Total Candidates', candidates_count)
+
+    consumer = create_kafka_consumer(topic_name)
+    data = fetch_data_from_kafka(consumer)
+
+    results = pd.DataFrame(data)
+
+    # Identify leading candidate
+    results = results.loc[results.groupby('candidate_id')['total_votes'].idxmax()]
+    
+
+
+st.title('Realtime Election Voting Dashboard')
+topic_name = 'aggregated_votes_per_candidate'
+
+if __name__ == '__main__':
+    update_data()
